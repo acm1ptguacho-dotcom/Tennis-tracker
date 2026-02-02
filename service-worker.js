59e80d8fd2a5fc188a21f603e37be1bf2dce2c2f
acm1ptguacho-dotcom <acm1ptguacho@gmail.com>
2026-02-02 22:56:09 +0100

    v52

--- service-worker.js
+++ service-worker.js
@@ -4,12 +4,12 @@
 // - JS/CSS: stale-while-revalidate
 // - Imágenes: cache-first
 
-const CACHE = "tennis-tracker-web-v2.53-premium-refresh";
+const CACHE = "tennis-tracker-web-v2.52-remove-modes";
 const CORE = [
   "./",
   "./index.html",
-  "./style.css?v=2530",
-  "./app.js?v=2530",
+  "./style.css?v=2520",
+  "./app.js?v=2520",
   "./assets/court_top_view.png",
   "./assets/board_court.png",
 ];
