e576bdaeac9b8afdaf6add49f6a74cb4a5e9e57f
acm1ptguacho-dotcom <acm1ptguacho@gmail.com>
2026-04-04 01:59:04 +0200

    sd

--- app.js
+++ app.js
@@ -4294,7 +4294,7 @@ function showSplashAgain(){
 
 function registerSW(){
   if (!("serviceWorker" in navigator)) return;
-  navigator.serviceWorker.register("./service-worker.js?v=2541").catch(console.error);
+  navigator.serviceWorker.register("./service-worker.js?v=2542").catch(console.error);
 }
 
 function init(){
