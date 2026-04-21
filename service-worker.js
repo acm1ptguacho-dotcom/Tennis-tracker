2a4da23b50a10c18a8dbc882f77ec09ce4435059
acm1ptguacho-dotcom <acm1ptguacho@gmail.com>
2026-04-21 06:33:02 +0200

    ass

--- service-worker.js
+++ service-worker.js
@@ -8,8 +8,8 @@ const CACHE = "tennis-tracker-web-v2.83-players-form-fix";
 const CORE = [
   "./",
   "./index.html",
-  "./style.css?v=2830",
-  "./app.js?v=2830",
+  "./style.css?v=2840",
+  "./app.js?v=2840",
   "./assets/logo.png",
   "./assets/court_top_view.png",
   "./assets/court_clay.png",
